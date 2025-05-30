ea06523f8f6fb81139d1044a196b8bab8e70e611
MrTrakez <111070577+MrTrakez@users.noreply.github.com>
2025-05-30 22:04:35 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,11 @@
+[
+    {
+      "crewmate": "Was ist dein Lieblingsobst?",
+      "impostor": "Was ist dein Lieblingsgemüse?"
+    },
+    {
+      "crewmate": "Welches Tier würdest du gerne als Haustier haben?",
+      "impostor": "Welches Tier würdest du gerne im Zoo besuchen?"
+    }
+    // ... weitere Fragepaare ...
+  ]
